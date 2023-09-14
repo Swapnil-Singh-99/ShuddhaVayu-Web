d4d3544f31351d3d793a7c2fd7149ae989a92fd0
Swapnil Singh <swapnilskumars@gmail.com>
2023-09-14 23:37:25 +0530

    added home page

---
+++ Client/src/components/homePage/homePage.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../navbar/navbar"
+import LoginPage from "../loginPage/loginPage"
+
+const HomePage = () => {
+    return (
+        <>
+            <Navbar />
+            <LoginPage/>
+        </>
+    )
+}
+
+export default HomePage
