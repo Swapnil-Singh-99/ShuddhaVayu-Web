63099651f0c8e091e765d77df23b79580ba2dd70
Swapnil Singh <swapnilskumars@gmail.com>
2023-09-14 23:37:51 +0530

    added admin login page

---
+++ Client/src/components/adminLoginPage/adminLoginPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../navbar/navbar';
+import AdminLogin from "../adminLogin/adminLogin"
+
+function AdminLoginPage() {
+    return (
+        <>
+            <Navbar />
+            <AdminLogin />
+        </>
+    );
+}
+
+export default AdminLoginPage;
