5defd7b930438ffe34010de968afa7227f104606
Swapnil Singh <swapnilskumars@gmail.com>
2023-09-15 01:30:18 +0530

    added button link for testing

--- Client/src/components/adminLogin/adminLogin.jsx
+++ Client/src/components/adminLogin/adminLogin.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import "./adminLogin.css"
 import accountCircle from "../../assets/account_circle.svg"
 import shieldLock from "../../assets/shield_lock.svg"
+import { Link } from 'react-router-dom';
 
 function AdminLogin() {
     return (
@@ -11,7 +12,7 @@ function AdminLogin() {
                 <div class="loginDiv">
                     <div class="username inputField"><img src={accountCircle} alt=""/><input type="text" placeholder="Username"/></div>
                     <div class="password inputField"><img src={shieldLock} alt=""/><input type="password" placeholder="Password"/></div>
-                    <button class="loginBtn">Login</button>
+                    <button class="loginBtn"><Link to="/dashboard" className="button-link">Login</Link></button>
                 </div>
             </div>
         </div>
