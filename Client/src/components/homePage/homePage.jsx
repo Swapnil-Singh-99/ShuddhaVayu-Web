2fa5955c14562c146ed581c8ce46ed8953746a62
HarshSharma0801 <121893196+HarshSharma0801@users.noreply.github.com>
2023-09-17 04:13:25 +0530

    Landing Page

--- Client/src/components/homePage/homePage.jsx
+++ Client/src/components/homePage/homePage.jsx
@@ -1,10 +1,11 @@
 import Navbar from "../navbar/navbar"
 import LoginPage from "../loginPage/loginPage"
+import homePage from "./LandingPage"
 
 const HomePage = () => {
     return (
         <>
-           
+           <HomePage/>
         </>
     )
 }
