f18855e6884cc5cdd49934388d92a3853e8d0e07
Swapnil Singh <swapnilskumars@gmail.com>
2023-09-25 00:18:54 +0530

    changed tagline

--- Client/src/components/homePage/LandingPage.jsx
+++ Client/src/components/homePage/LandingPage.jsx
@@ -5,13 +5,14 @@ const LandingPage = () => {
 
   return (
     <div className="bg-Main min-h-screen flex items-center justify-center">
-      <div className="text-white text-center">
-      <h1 className="text-5xl font-bold mb-4 py-5">Shuddhavayu</h1>
+      <div className="text-white text-center justify-center flex flex-col">
+        {/* <img className="text-5xl font-bold mb-4 py-5 w-0" src="https://i.postimg.cc/CLpt6K0y/SIH2023-logo-1.png" alt="My Image" /> */}
+        <h1 className="text-5xl font-bold mb-4 py-5">Shuddhavayu</h1>
 
-        <h1 className="text-4xl font-bold mb-4">Welcome,  to Our Website</h1>
-        <p className="text-lg mb-8">Explore and discover amazing content.</p>
+        <h1 className="text-4xl font-bold mb-4">Making a pollution free Bharat</h1>
+        <p className="text-lg mb-8">#PradushanRahitBharat</p>
         <div className='flex justify-center'>
-        {/* <div>
+          {/* <div>
         <a
           href="#"
           className="bg-white text-Main hover:bg-Main hover:text-white px-6 py-3 rounded-full text-lg font-semibold transition duration-300 ease-in-out"
@@ -20,18 +21,18 @@ const LandingPage = () => {
         </a>
 
         </div> */}
-        <div>
-        <a
-          href="/adminlogin"
-          className="bg-white text-Main hover:bg-Main hover:text-white px-6 py-3 rounded-full text-lg font-semibold transition duration-300 ease-in-out"
-        >
-          Admin Login
-        </a>
-        </div>
+          <div>
+            <a
+              href="/adminlogin"
+              className="bg-white text-Main hover:bg-Main hover:text-white px-6 py-3 rounded-full text-lg font-semibold transition duration-300 ease-in-out"
+            >
+              Admin Login
+            </a>
+          </div>
 
         </div>
-        
-       
+
+
       </div>
     </div>
   );
